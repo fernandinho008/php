c2e75ca7de3a533fb494c83a1d9ce149c42757b1
FERNANDO AUGUSTO <141058274+fernandinho008@users.noreply.github.com>
2024-05-22 14:49:53 -0300

    exercício var int

---
+++ aulas.php/exercício_var_int.php
@@ -0,0 +1,17 @@
+<?php 
+//Declaração de uma variável de número inteiro
+$idadeAluno = 18;
+
+?>
+
+<html>
+    <head>
+        <title>Tipo inteiro de php</title>
+    </head>
+
+
+<body>
+    
+<p>O aluno tem <?php echo $idadeAluno;  ?> de idade.</p>
+</body>
+</html>
